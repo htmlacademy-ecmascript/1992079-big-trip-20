4793f8f59bdb7d4adf7e2f8a37a3f8f293866239
4rutemu <artyom.denega@gmail.com>
2023-05-17 16:13:13 +0300

    added mock data and model

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,9 +1,9 @@
-import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
-import CreateFormView from './view/create-form-view.js';
-import EditFormView from './view/edit-form-view.js';
-import PointView from './view/point-view.js';
-import { render } from './render.js';
+import FilterView from '../view/filter-view.js';
+import SortView from '../view/sort-view.js';
+import CreateFormView from '../view/create-form-view.js';
+import EditFormView from '../view/edit-form-view.js';
+import PointView from '../view/point-view.js';
+import { render } from '../render.js';
 
 const POINTS_COUNT = 3;
 
