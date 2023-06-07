329608cea308ee073bf683cb4c4501ec02b157d9
4rutemu <artyom.denega@gmail.com>
2023-06-07 13:21:12 +0300

    upd point view: fix on destination name

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -6,7 +6,7 @@ const createPointTemplate = (props) =>
   <div class="event__type">
     <img class="event__type-icon" width="42" height="42" src="img/icons/${props.type.toLowerCase()}.png" alt="Event type icon">
   </div>
-  <h3 class="event__title">${props.type} ${props.destination}</h3>
+  <h3 class="event__title">${props.type} ${props.destination.cityName}</h3>
   <div class="event__schedule">
     <p class="event__time">
       <time class="event__start-time" datetime="${props.startDate}">${props.startDate.getHours()}:${props.startDate.getMinutes()}</time>
